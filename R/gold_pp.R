138b32b0117c5cea5e5e49f78e23afd56398ae49
reykp <kathleenrey89@gmail.com>
2018-10-18 22:20:52 -0500

    d

--- R/gold_pp.R
+++ R/gold_pp.R
@@ -34,38 +34,46 @@
 
 
 
-gold_pp <- function(gold_output, npar=9, burnin=NA){
+gold_pp <- function(gold_output, npar=6, burnin=NA){
   
+  # Get the matrix of posterior simulations
   G_output <- gold_output$G
   
+  # If the burnin is not specified, set to have the iterations
   if(is.na(burnin)){
     burnin <- ceiling(nrow(G_output)/2)
   }
   
+  # Calculate the number of distance between parameters
   npar_intervals <- floor(ncol(G_output)/npar)
   
+  # Select the parameters to plot
   parameters <- NA
   parameters[1] <- 1
+  
+  if(npar>1){
   for(i in 2:npar){
     if((parameters[{i-1}] +npar_intervals)<ncol(G_output)){
       parameters[i] <- parameters[{i-1}] +npar_intervals
     }
   }
-  parameters <<- parameters
+  }
   
+  # Remove the burnin
   G <- data.frame(G_output[(burnin+1):nrow(G_output),parameters])
 
   names(G) <- parameters
+  # Create a data set with the posterior draws
   G_plot <- G %>% gather(Parameter, Simulation)
   G_plot$Parameter <- as.numeric(as.character(G_plot$Parameter))
   G_plot$Source <- "Posterior"
   
+  # Get the prior covariance parameters
   s1 <- gold_output$prior[which(names(gold_output$prior)=="s1")]
   c1 <- gold_output$prior[which(names(gold_output$prior)=="c1")]
-  s2 <- gold_output$prior[which(names(gold_output$prior)=="s2")]
-  c2 <- gold_output$prior[which(names(gold_output$prior)=="c2")]
   x <- gold_output$x
   
+  # Calculat the prior covariance
   cov_prior <- matrix(0, nrow=length(x), ncol=length(x))
   for (i in 1:length(x)){
     for(j in 1:length(x)){
@@ -82,10 +90,13 @@ gold_pp <- function(gold_output, npar=9, burnin=NA){
     }
   }
   
+  # Collect the subset of the prior covariance for the selected paramters
   cov_prior <- cov_prior[parameters, parameters]
   
-      n_x <<- length(gold_output$x[parameters])
+  # Find the number of paramters
+  n_x <- length(gold_output$x[parameters])
 
+  # For the inverse
       n <- 1
       mu <- rep(0, n_x)
       tol = 1e-06
@@ -95,38 +106,46 @@ gold_pp <- function(gold_output, npar=9, burnin=NA){
       if (!all(ev >= -tol * abs(ev[1L])))
         stop("'Sigma' is not positive definite")
       
-      mvrnorm_calc <<- eS$u %*% diag(sqrt(pmax(ev, 0)), p)
+      mvrnorm_calc <- eS$u %*% diag(sqrt(pmax(ev, 0)), p)
 
-            
+      
+      # Create data set to hold prior      
       G_prior <- data.frame(Source = rep("Prior", nrow(G)))
       
-      gold_prior <- function(x){
+      gold_prior <- function(x, n_x, mvrnorm_calc){
         rw <- matrix(rnorm(n_x * 1), 1)
         prior_sim <- mvrnorm_calc %*% t(rw)
         
         return(prior_sim)
       }
       
-      G_prior <- cbind(G_prior,t(apply(G_prior, 1, gold_prior)))
+      # Simulate from the prior distribution
+      G_prior <- cbind(G_prior,t(apply(G_prior, 1, gold_prior, n_x, mvrnorm_calc)))
       names(G_prior)[2:ncol(G_prior)] <- names(G)
       
+      # Prior for plotting
       G_plot_prior <- G_prior %>% gather(Parameter, Simulation, -Source)
       
-      G_plot <- rbind(G_plot, G_plot_prior)
+      #G_plot <- rbind(G_plot, G_plot_prior[,c("Parameter", "Simulation", "Source")])
       
       #G_plot$Parameter <- as.factor(gsub("X", "", G_plot$Parameter))
       
-      G_plot$Parameter <- as.factor(parameters)
-      G_plot$Parameter <- factor(G_plot$Parameter, levels=c(parameters))
+      G_plot$Parameter <- as.factor(G_plot$Parameter)
+      G_plot_prior$Parameter <- as.factor(G_plot_prior$Parameter)
+      G_plot$Parameter <- factor(G_plot$Parameter, levels = c(parameters))
+      G_plot_prior$Parameter <- factor(G_plot_prior$Parameter, levels = c(parameters))
       
-      graph_index <- ceiling(npar/9)
+      graph_index <- ceiling(npar/6)
       for (i in 1:graph_index) {
         
-        print(ggplot(G_plot, aes(Simulation,fill=Source, color=Source))+
-                geom_histogram(position = "identity", alpha = .5, bins = 60)+
+        print(ggplot()+
+                geom_histogram(data = G_plot, aes(Simulation),fill ="#F8766D", color = "#F8766D", alpha = .5, bins = 60)+
+                geom_histogram(data = G_plot_prior, aes(Simulation),fill ="#00BFC4", color = "#00BFC4", alpha = .5, bins = 60)+
                 theme_bw()+theme(axis.title = element_text(size = 12))+
-                facet_wrap_paginate(~Parameter, ncol = 3, nrow = 3, page = i))
+                facet_wrap_paginate(~Parameter, ncol = 2, nrow = 3, page = i))
         
         
-            }
+      }
+        
+        
 }
